dded45210d820f456f2e6bcd0865f33f5248e34d
itsamanverma6233-beep <itsamanverma6233@gmail.com>
2026-04-19 22:54:46 +0530

    Updated Feature

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Features from "./Components/Features.jsx"
+import Navbar from "./Components/Nav.jsx"
+
+function App(){
+  return(
+    <div>
+      <Navbar/>
+      <Features/>
+    </div>
+  )
+}
+export default App
